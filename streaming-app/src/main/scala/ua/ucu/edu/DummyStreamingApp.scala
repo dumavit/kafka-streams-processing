129365bd3f568bd96850d7e33a8ebde634a1e406
Dima Glushko <dima2492@gmail.com>
2019-03-13 21:48:14 +0200

    Commented not ready code.

--- streaming-app/src/main/scala/ua/ucu/edu/DummyStreamingApp.scala
+++ streaming-app/src/main/scala/ua/ucu/edu/DummyStreamingApp.scala
@@ -11,17 +11,17 @@ import org.slf4j.LoggerFactory
 // dummy app for testing purposes
 object DummyStreamingApp extends App {
 
-  val logger = LoggerFactory.getLogger(getClass)
-
-  val props = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming_app")
-  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(Config.KafkaBrokers))
-  props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, Long.box(5 * 1000))
-  props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, Long.box(0))
-
-  import Serdes._
-
-  val builder = new StreamsBuilder
+//  val logger = LoggerFactory.getLogger(getClass)
+//
+//  val props = new Properties()
+//  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming_app")
+//  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(Config.KafkaBrokers))
+//  props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, Long.box(5 * 1000))
+//  props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, Long.box(0))
+//
+//  import Serdes._
+//
+//  val builder = new StreamsBuilder
 
   /*val testStream = builder.stream[String, String]("weather_data")
 
@@ -39,23 +39,23 @@ object DummyStreamingApp extends App {
     streams.close(10, TimeUnit.SECONDS)
   }*/
 
-  implicit val weatherSerde: Serde[Weather] = fromFn(View.serialize _, Weather.deserialize _)
-  implicit val panelSerde: Serde[Panel] = fromFn(Panel.serialize _, Panel.deserialize _)
-  implicit val panelResultSerde: Serde[PanelResult] = fromFn(PanelResult.serialize _, PanelResult.deserialize _)
-
-  val builder = new StreamsBuilder
-  val panels = builder.stream[String, Panel]("panel-data")
-    .selectKey((_, panel) => panel.location)
-  val weather: KTable[String, Weather] = builder.stream[String, Weather]("weather_data")
-    .groupBy((_, weather) => weather.location)
-    .reduce((prev, next) => {if (next.time > prev.time) next else prev})
-  val result = panels.leftJoin(weather)((_, weather) => PanelResult(weather.data, weather.location))
-
-  result.to("panel-result")
-  val topology = builder.build()
-  topology
-
-  object Config {
-    val KafkaBrokers = "KAFKA_BROKERS"
-  }
+//  implicit val weatherSerde: Serde[Weather] = fromFn(View.serialize _, Weather.deserialize _)
+//  implicit val panelSerde: Serde[Panel] = fromFn(Panel.serialize _, Panel.deserialize _)
+//  implicit val panelResultSerde: Serde[PanelResult] = fromFn(PanelResult.serialize _, PanelResult.deserialize _)
+//
+//  val builder = new StreamsBuilder
+//  val panels = builder.stream[String, Panel]("panel-data")
+//    .selectKey((_, panel) => panel.location)
+//  val weather: KTable[String, Weather] = builder.stream[String, Weather]("weather_data")
+//    .groupBy((_, weather) => weather.location)
+//    .reduce((prev, next) => {if (next.time > prev.time) next else prev})
+//  val result = panels.leftJoin(weather)((_, weather) => PanelResult(weather.data, weather.location))
+//
+//  result.to("panel-result")
+//  val topology = builder.build()
+//  topology
+//
+//  object Config {
+//    val KafkaBrokers = "KAFKA_BROKERS"
+//  }
 }
--- weather-provider/src/main/scala/ua/ucu/edu/Main.scala
+++ weather-provider/src/main/scala/ua/ucu/edu/Main.scala
@@ -10,23 +10,23 @@ import scala.language.postfixOps
 
 object Main extends App {
 
-  val logger = LoggerFactory.getLogger(getClass)
-
-  logger.info("======== Weather Provider App Init ========")
-
-  val system = ActorSystem()
-  import system.dispatcher
-
-  import duration._
-
-  system.scheduler.schedule(5 seconds, 10 seconds, new Runnable {
-    override def run(): Unit = {
-      logger.debug("weather request")
-      // ???
-      // todo - ask weather api and send data to kafka topic - recommended format is json - or you can come up with simpler string-based protocol
-    }
-  })
-
-  // for testing purposes only
-  DummyDataProducer.pushTestData()
+//  val logger = LoggerFactory.getLogger(getClass)
+//
+//  logger.info("======== Weather Provider App Init ========")
+//
+//  val system = ActorSystem()
+//  import system.dispatcher
+//
+//  import duration._
+//
+//  system.scheduler.schedule(5 seconds, 10 seconds, new Runnable {
+//    override def run(): Unit = {
+//      logger.debug("weather request")
+//      // ???
+//      // todo - ask weather api and send data to kafka topic - recommended format is json - or you can come up with simpler string-based protocol
+//    }
+//  })
+//
+//  // for testing purposes only
+//  DummyDataProducer.pushTestData()
 }
