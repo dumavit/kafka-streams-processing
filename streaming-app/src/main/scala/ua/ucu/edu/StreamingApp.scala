d26736a1ff1b3469937b1e6ae9167382398bbd3b
Dima Glushko <dima2492@gmail.com>
2019-03-18 00:18:17 +0200

    Fixed streaming app

--- streaming-app/src/main/scala/ua/ucu/edu/StreamingApp.scala
+++ streaming-app/src/main/scala/ua/ucu/edu/StreamingApp.scala
@@ -2,12 +2,12 @@ package ua.ucu.edu
 
 import java.util.Properties
 
-import org.apache.kafka.streams.kstream.{Consumed, Joined, Produced}
+import org.apache.kafka.streams.kstream._
 import ua.ucu.edu.model._
 import org.apache.kafka.streams.scala.kstream.KTable
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
-import org.apache.kafka.common.serialization.{Serdes, Serde}
+import org.apache.kafka.common.serialization.{Serde, Serdes}
 
 
 class StreamingApp {
@@ -21,16 +21,25 @@ class StreamingApp {
 
     val builder = new StreamsBuilder
     val sensorData = builder
-      .stream[String, SensorRecord]("sensor-data")(Consumed.`with`(Serdes.String(), sensorRecordSerde))
+      .stream[String, SensorRecord]("sensor-data")(Consumed.`with`(Serdes.serdeFrom(classOf[String]), sensorRecordSerde))
+      .selectKey((_, sensorRecord) => s"${sensorRecord.location.latitude}:${sensorRecord.location.longitude}")
 
     val weatherData: KTable[String, WeatherRecord] = builder
-      .table[String, WeatherRecord]("weather-data")(Consumed.`with`(Serdes.String(), weatherRecordSerde))
+      .stream[String, WeatherRecord]("weather-data")(Consumed.`with`(Serdes.serdeFrom(classOf[String]), weatherRecordSerde))
+      .groupBy(
+        (_, weatherRecord) =>
+          s"${weatherRecord.location.latitude}:${weatherRecord.location.longitude}"
+      )(Serialized.`with`(Serdes.serdeFrom(classOf[String]), weatherRecordSerde))
+      .reduce(
+        (prev, next) =>
+          next
+      )(Materialized.`with`(Serdes.serdeFrom(classOf[String]), weatherRecordSerde))
 
-    sensorData.join(weatherData)((sensorRecord, weatherRecord) => {
+    sensorData.leftJoin(weatherData)((sensorRecord, weatherRecord) => {
       MergedRecord(sensorRecord.panelId, sensorRecord.location, sensorRecord.measurements,
         sensorRecord.timestamp, weatherRecord.temperature, weatherRecord.humidity)
-    })(Joined.`with`(Serdes.String(), sensorRecordSerde, weatherRecordSerde))
-      .to("merged-data")(Produced.`with`(Serdes.String(), mergedRecordSerde))
+    })(Joined.`with`(Serdes.serdeFrom(classOf[String]), sensorRecordSerde, weatherRecordSerde))
+      .to("merged-data")(Produced.`with`(Serdes.serdeFrom(classOf[String]), mergedRecordSerde))
 
     val topology = builder.build()
     topology
@@ -42,7 +51,7 @@ object StreamingApp extends App {
   val topic = "sensor-data"
   val props = new Properties()
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
-  props.put(StreamsConfig.CLIENT_ID_CONFIG, "streaming-app-1")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming-app-1")
   props.put(StreamsConfig.RETRIES_CONFIG, "5")
 
   val app = new StreamingApp
