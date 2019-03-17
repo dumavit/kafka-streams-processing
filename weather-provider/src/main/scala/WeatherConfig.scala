7e41b4132ca172af4e4f96a0b5c5834a3a9d3853
dumavit <duma1492@gmail.com>
2019-03-17 13:15:49 +0200

    Add kafka producer for weather data

---
+++ weather-provider/src/main/scala/WeatherConfig.scala
@@ -0,0 +1,6 @@
+package ua.ucu.edu
+
+object WeatherConfig {
+  val WEATHER_PROVIDER_APP_ID = "e36672e3b1143cc4d64650d471185970"
+  val WEATHER_PROVIDER_URL = "https://api.openweathermap.org/data/2.5/weather"
+}
--- weather-provider/src/main/scala/ua/ucu/edu/provider/WeatherProviderApi.scala
+++ weather-provider/src/main/scala/ua/ucu/edu/provider/WeatherProviderApi.scala
@@ -1,8 +1,31 @@
 package ua.ucu.edu.provider
 
 import ua.ucu.edu.model._
+import dispatch._
+import Defaults._
+import play.api.libs.json._
+import ua.ucu.edu.WeatherConfig
+
 
 trait WeatherProviderApi {
+  def weatherAtLocation(location: Location): Future[WeatherRecord]
+}
 
-  def weatherAtLocation(location: Location): WeatherData
+
+object WeatherProvider extends WeatherProviderApi {
+  def weatherAtLocation(loc: Location): Future[WeatherRecord] = {
+    val query = s"lat=${loc.latitude}&lon=${loc.longitude}&appId=${WeatherConfig.WEATHER_PROVIDER_APP_ID}"
+    val svc = url(s"${WeatherConfig.WEATHER_PROVIDER_URL}?$query")
+    val response: Future[String] = Http.default(svc OK as.String)
+
+    response map {
+      resp => {
+        val jsvalue: JsValue = Json.parse(resp)
+        val temperature: Double = (jsvalue \ "main" \ "temp").get.as[Double]
+        val humidity: Double = (jsvalue \ "main" \ "humidity").get.as[Double]
+
+        WeatherRecord(loc, temperature, humidity)
+      }
+    }
+  }
 }
