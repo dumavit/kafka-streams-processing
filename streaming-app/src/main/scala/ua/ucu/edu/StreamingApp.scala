54d6ac39a25700ff7ac04793c920b1a2496a6467
Dima Glushko <dima2492@gmail.com>
2019-03-17 23:34:31 +0200

    Fixed streaming app

--- streaming-app/src/main/scala/ua/ucu/edu/StreamingApp.scala
+++ streaming-app/src/main/scala/ua/ucu/edu/StreamingApp.scala
@@ -2,12 +2,11 @@ package ua.ucu.edu
 
 import java.util.Properties
 
+import org.apache.kafka.streams.kstream.{Consumed, Joined, Produced}
 import ua.ucu.edu.model._
 import org.apache.kafka.streams.scala.kstream.KTable
-import org.apache.kafka.streams.scala.ImplicitConversions._
-import org.apache.kafka.streams.scala.{Serdes, StreamsBuilder}
+import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
-import Serdes._
 import org.apache.kafka.common.serialization.{Serdes, Serde}
 
 
@@ -22,15 +21,16 @@ class StreamingApp {
 
     val builder = new StreamsBuilder
     val sensorData = builder
-      .stream[String, SensorRecord]("sensor-data")
+      .stream[String, SensorRecord]("sensor-data")(Consumed.`with`(Serdes.String(), sensorRecordSerde))
 
     val weatherData: KTable[String, WeatherRecord] = builder
-      .table[String, WeatherRecord]("weather-data")
+      .table[String, WeatherRecord]("weather-data")(Consumed.`with`(Serdes.String(), weatherRecordSerde))
 
     sensorData.join(weatherData)((sensorRecord, weatherRecord) => {
       MergedRecord(sensorRecord.panelId, sensorRecord.location, sensorRecord.measurements,
         sensorRecord.timestamp, weatherRecord.temperature, weatherRecord.humidity)
-    }).to("merged-data")
+    })(Joined.`with`(Serdes.String(), sensorRecordSerde, weatherRecordSerde))
+      .to("merged-data")(Produced.`with`(Serdes.String(), mergedRecordSerde))
 
     val topology = builder.build()
     topology
